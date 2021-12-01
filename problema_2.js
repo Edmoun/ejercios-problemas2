aed485e3819a58e2eda4cd3d34d64447e1e8a01d
Edmoun <edmounramirez@gmail.com>
2021-12-01 11:21:32 -0400

    Problema_2

---
+++ problema_2.js
@@ -0,0 +1,15 @@
+let num = 9;
+let sum = 0;
+
+while(!(num == 0) ) {
+    sum += num;
+    --num;
+}
+
+console.log(sum);
+
+// Hecho por gaus
+
+let num=9;
+let sum_gaus = num * (num + 1)/2
+console.log(sum_gaus);
