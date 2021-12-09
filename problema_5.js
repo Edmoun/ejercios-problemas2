7a1cb036214efe60954d718a840f367252f78fc7
Edmoun <edmounramirez@gmail.com>
2021-12-08 22:23:27 -0400

    problema_5

---
+++ problema_5.js
@@ -0,0 +1,16 @@
+var num1 = 1 * prompt ("Introduce el primer numero");
+
+var num2 = 1 * prompt ("Introduce el segundo numero");
+
+result = 0;
+
+if (num1 <= 0 || num2 <= 0) {
+    alert ("ERROR (Los dos numeros deben ser positivos)");
+} else {
+var i = 0;
+    while (i < num2){
+        result += num1;
+        i++;
+    }
+    alert ("Este es el resultado " + result);
+}
