c6fcd5a4292235f1eb7688fc58d21d8dddb424e7
Edmoun <edmounramirez@gmail.com>
2021-12-11 22:43:23 -0400

    problema_11

---
+++ problema_12.js
@@ -0,0 +1,11 @@
+var cdig = function(cantidad, digito){
+    var contador = 0;
+    for(var i = 0; i < cantidad.length; i++){
+        if(cantidad[i] == digito){
+            contador++;
+        }
+    }
+    return contador;
+}
+
+cdig (1241, 1);
