b223fefd770e42139aef5a00987b8efc457441e5
Edmoun <edmounramirez@gmail.com>
2021-12-11 23:06:19 -0400

    problema_20

---
+++ problema_20.js
@@ -0,0 +1,12 @@
+
+var n = prompt("Ingrese el número de latas");
+var niveles = 0;
+var sobrantes = 0;
+function triangulo(n) {
+    for(var i = 1; i <= n; i++) {
+        niveles += i;
+    }
+    sobrantes = n - niveles;
+    console.log("El triángulo tiene " + niveles + " niveles y " + sobrantes + " latas sobrantes");
+}
+triangulo(n);
